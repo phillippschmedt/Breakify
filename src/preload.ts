4f6b1d1adf0e707bcf2c0f2ec057f0f6e3487e35
Phillipp Schmedt <phillipp.schmedt@gmail.com>
2020-10-11 18:50:40 +0200

    Remove unused example code from preload.ts

--- src/preload.ts
+++ src/preload.ts
@@ -2,20 +2,6 @@
 
 import { ipcRenderer } from "electron";
 
-// It has the same sandbox as a Chrome extension.
-window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector: string, text: string) => {
-    const element = document.getElementById(selector);
-    if (element) {
-      element.innerText = text;
-    }
-  };
-
-  for (const type of ["chrome", "node", "electron"]) {
-    replaceText(`${type}-version`, (process.versions as any)[type]);
-  }
-});
-
 window.ipcRenderer = ipcRenderer
 
 
