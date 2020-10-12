ab9f14e01a66319abe1e947b37b18eb919c2600f
Phillipp Schmedt <phillipp.schmedt@gmail.com>
2020-10-12 14:07:20 +0200

    Add more comments

--- src/preload.ts
+++ src/preload.ts
@@ -1,5 +1,5 @@
 // All of the Node.js APIs are available in the preload process.
-
 import { ipcRenderer } from "electron";
 
+// Expose the ipcRenderer to the render process
 window.ipcRenderer = ipcRenderer
